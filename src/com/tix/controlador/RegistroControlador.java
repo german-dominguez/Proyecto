6d2479175907f2fa3eb42ea6a46916e21fb73c6c
agust <agust@Agus>
2022-10-28 10:40:17 -0300

    Cambios en el login

--- src/com/tix/controlador/RegistroControlador.java
+++ src/com/tix/controlador/RegistroControlador.java
@@ -82,6 +82,9 @@ public class RegistroControlador {
 	}
 
 	public void registrarAnalista() {
+		
+		System.out.println(itrsBeanRemote.obtenerItrPorId((long) 1));
+		
 		Analista analista = new Analista();
 
 		String emailInstitucional = vista.getTxtEmailInstitucional();
@@ -131,6 +134,7 @@ public class RegistroControlador {
 		estudiante.setEstado(0);
 		estudiante.setGenero("masculino");
 		estudiante.setItr(itrsBeanRemote.obtenerItrPorId((long) 1));
+		System.out.println(itrsBeanRemote.obtenerItrPorId((long) 1).getNombre());
 		estudiante.setLocalidad(localidadesBeanRemote.obtenerLocalidadPorId((long) 1));
 
 		estudiantesBeanRemote.registro(estudiante);
